95179344eb66a90d30b48ac558df578e16d552fe
Petter1307 <petrealupu75@gmail.com>
2022-01-10 11:45:53 +0200

    Added pages and basic code

---
+++ lib/pages/plant.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlantContainer extends StatefulWidget {
+  const PlantContainer({ Key? key }) : super(key: key);
+
+  @override
+  _PlantContainerState createState() => _PlantContainerState();
+}
+
+class _PlantContainerState extends State<PlantContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
