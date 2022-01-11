313183549aa5dc3d2b0de585070e448eb871029f
Petter1307 <petrealupu75@gmail.com>
2022-01-11 20:48:01 +0200

    Added container -- minor bugs

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ void main() {
   runApp(MaterialApp(
     initialRoute: '/', // this should be loading page, for testing purposes now this is the homepage.
     routes: {
-      '/' : (context) => Home(), // The homepage WORKINGPROGRESS. 
+     // '/' : (context) => Home(), // The homepage WORKINGPROGRESS. 
       // '/home' : (context) => Loading(), // To be added later. Loading page.
-      // '/plant' : (context) => PlantContainer() // To be added later. Page for each plant 
+       '/' : (context) => PlantContainer() // To be added later. Page for each plant 
     },
   ));
 }
--- lib/pages/plant.dart
+++ lib/pages/plant.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:plantonizer/classes/plant_class.dart';
 
+
 class PlantContainer extends StatefulWidget {
   const PlantContainer({ Key? key }) : super(key: key);
 
@@ -11,8 +12,30 @@ class PlantContainer extends StatefulWidget {
 class _PlantContainerState extends State<PlantContainer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: SafeArea(
+        child:Wrap(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                color: Colors.grey[500],
+                height: 250,
+                width: 250,
+              //   decoration: BoxDecoration(
+              //  // border: Border.all(
+              //   //color: Colors.red[500],        // this is bugged out. doesn`t work as intended.
+              //   // ),
+              //   borderRadius: BorderRadius.circular(20) // use instead of BorderRadius.all(Radius.circular(20))
+              //   ),
+              // decoration: BoxDecoration(
+              //   border: Border(bottom: 2,),
+              // ),
+              ),
+            ),
+          ],
+        ),
+        ),
     );
   }
 }
